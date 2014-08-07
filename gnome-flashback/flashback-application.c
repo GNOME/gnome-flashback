fd23b328d6d521e789dbd9b27f77ec9a77bade33
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2014-08-08 00:39:29 +0300

    enable end session dialog

--- gnome-flashback/flashback-application.c
+++ gnome-flashback/flashback-application.c
@@ -20,14 +20,116 @@
 #include "config.h"
 #include "flashback-application.h"
 #include "flashback-desktop-background.h"
+#include "flashback-end-session-dialog-generated.h"
+#include "flashback-inhibit-dialog.h"
 #include "flashback-gsettings.h"
 
 struct _FlashbackApplicationPrivate {
 	FlashbackDesktopBackground *background;
 	GSettings                  *settings;
+	gint                        bus_name;
 };
 
 G_DEFINE_TYPE (FlashbackApplication, flashback_application, GTK_TYPE_APPLICATION);
+
+static void
+inhibit_dialog_response (FlashbackInhibitDialog    *dialog,
+                         guint                      response_id,
+                         FlashbackEndSessionDialog *object)
+{
+	int action;
+
+	g_object_get (dialog, "action", &action, NULL);
+	gtk_widget_destroy (GTK_WIDGET (dialog));
+
+	switch (response_id) {
+	case GTK_RESPONSE_CANCEL:
+	case GTK_RESPONSE_NONE:
+	case GTK_RESPONSE_DELETE_EVENT:
+		if (action == FLASHBACK_LOGOUT_ACTION_LOGOUT
+			|| action == FLASHBACK_LOGOUT_ACTION_SHUTDOWN
+			|| action == FLASHBACK_LOGOUT_ACTION_REBOOT) {
+			g_print ("cancel action %d\n", action);
+			flashback_end_session_dialog_emit_canceled (object);
+			flashback_end_session_dialog_emit_closed (object);
+		}
+		break;
+	case GTK_RESPONSE_ACCEPT:
+		g_print ("confirm action %d\n", action);
+		if (action == FLASHBACK_LOGOUT_ACTION_LOGOUT) {
+			flashback_end_session_dialog_emit_confirmed_logout (object);
+		} else if (action == FLASHBACK_LOGOUT_ACTION_SHUTDOWN) {
+			flashback_end_session_dialog_emit_confirmed_shutdown (object);
+		} else if (action == FLASHBACK_LOGOUT_ACTION_REBOOT) {
+			flashback_end_session_dialog_emit_confirmed_reboot (object);
+		}
+		break;
+	default:
+		g_assert_not_reached ();
+		break;
+	}
+}
+
+static gboolean
+handle_open (FlashbackEndSessionDialog *object,
+             GDBusMethodInvocation     *invocation,
+             guint                      arg_type,
+             guint                      arg_timestamp,
+             guint                      arg_seconds_to_stay_open,
+             const gchar *const        *arg_inhibitor_object_paths,
+             gpointer                   user_data)
+{
+	g_print ("handle open\n");
+
+	GtkWidget *dialog = flashback_inhibit_dialog_new (arg_type, arg_seconds_to_stay_open, arg_inhibitor_object_paths);
+
+	g_signal_connect (dialog, "response", G_CALLBACK (inhibit_dialog_response), object);
+
+	gtk_window_present_with_time (GTK_WINDOW (dialog), arg_timestamp);
+
+	flashback_end_session_dialog_complete_open (object, invocation);
+
+	return TRUE;
+}
+
+static void
+on_bus_acquired (GDBusConnection *connection,
+                 const gchar     *name,
+                 gpointer         user_data)
+{
+	g_print ("Acquired a message bus connection\n");
+
+	GDBusInterfaceSkeleton *iface;
+	GError *error = NULL;
+
+	iface = G_DBUS_INTERFACE_SKELETON (flashback_end_session_dialog_skeleton_new ());
+	g_signal_connect (iface, "handle-open", G_CALLBACK (handle_open), NULL);
+
+	if (!g_dbus_interface_skeleton_export (iface,
+	                                       connection,
+	                                       "/org/gnome/SessionManager/EndSessionDialog",
+	                                       &error)) {
+		g_warning ("Failed to export interface: %s", error->message);
+		g_error_free (error);
+		return;
+	}
+}
+
+static void
+on_name_acquired (GDBusConnection *connection,
+                  const gchar     *name,
+                  gpointer         user_data)
+{
+	g_print ("Acquired the name %s\n", name);
+}
+
+static void
+on_name_lost (GDBusConnection *connection,
+              const gchar     *name,
+              gpointer         user_data)
+{
+	g_print ("Lost the name %s\n", name);
+}
 
 static void
 flashback_application_settings_changed (GSettings   *settings,
@@ -71,6 +173,16 @@ flashback_application_startup (GApplication *application)
 	                  G_CALLBACK (flashback_application_settings_changed), app);
 	flashback_application_settings_changed (app->priv->settings, KEY_DRAW_BACKGROUND, app);
 
+	app->priv->bus_name = g_bus_own_name (G_BUS_TYPE_SESSION,
+	                                      "org.gnome.Shell",
+	                                      G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT |
+	                                      G_BUS_NAME_OWNER_FLAGS_REPLACE,
+	                                      on_bus_acquired,
+	                                      on_name_acquired,
+	                                      on_name_lost,
+	                                      NULL,
+	                                      NULL);
+
 	g_application_hold (application);
 }
 
@@ -88,6 +200,8 @@ flashback_application_shutdown (GApplication *application)
 		g_object_unref (app->priv->settings);
 		app->priv->settings = NULL;
 	}
+
+	g_bus_unown_name (app->priv->bus_name);
 
 	G_APPLICATION_CLASS (flashback_application_parent_class)->shutdown (application);
 }
