b757b7bcb37056a3ffa1ace41f6abe5e6f6b9ceb
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2015-11-29 20:58:07 +0200

    Revert "display-config: ignore outputs that has no modes"
    This reverts commit c375d0f042dd16babf6c930a626e1a69f58f3d85.

--- gnome-flashback/libdisplay-config/flashback-monitor-manager.c
+++ gnome-flashback/libdisplay-config/flashback-monitor-manager.c
@@ -1258,7 +1258,7 @@ read_current_config (FlashbackMonitorManager *manager)
 
       meta_output = &manager->outputs[n_actual_outputs];
 
-      if (output->connection != RR_Disconnected && output->nmode != 0)
+      if (output->connection != RR_Disconnected)
         {
           GBytes *edid;
 
