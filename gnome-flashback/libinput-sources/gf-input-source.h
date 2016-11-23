3ca1700d7bbe18a4a813338f2ede489dd2851a1a
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2016-11-23 09:54:09 +0200

    Revert "input-sources: add gf_input_source_generate_svg"
    This reverts commit 34416e64e42bdd1ff0a87eb8f79a4f66d47e8861.

--- gnome-flashback/libinput-sources/gf-input-source.h
+++ gnome-flashback/libinput-sources/gf-input-source.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2015 Sebastian Geiger
- * Copyright (C) 2016 Alberts Muktupāvels
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,8 +22,6 @@
 #include <ibus-1.0/ibus.h>
 
 #include "gf-ibus-manager.h"
-
-G_BEGIN_DECLS
 
 #define GF_TYPE_INPUT_SOURCE gf_input_source_get_type ()
 G_DECLARE_FINAL_TYPE (GfInputSource, gf_input_source,
@@ -59,14 +56,5 @@ IBusPropList  *gf_input_source_get_properties   (GfInputSource *source);
 
 void           gf_input_source_set_properties   (GfInputSource *source,
                                                  IBusPropList  *prop_list);
-
-GString       *gf_input_source_generate_svg     (GfInputSource *source,
-                                                 const gchar   *font_family,
-                                                 gint           font_size,
-                                                 const gchar   *bg_color,
-                                                 const gchar   *fg_color,
-                                                 gboolean       symbolic);
-
-G_END_DECLS
 
 #endif
