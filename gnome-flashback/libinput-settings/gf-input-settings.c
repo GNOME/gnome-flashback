c45734eeee305651d553633ad7be12b747bf5c68
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2017-07-21 18:54:26 +0300

    input-settings: monitor manager might not be available
    Check if monitor manager is available before using it otherwise
    we will get segfault.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=785227

--- gnome-flashback/libinput-settings/gf-input-settings.c
+++ gnome-flashback/libinput-settings/gf-input-settings.c
@@ -1038,6 +1038,9 @@ find_output (GfInputSettings *settings,
   if (!*edid[0] && !*edid[1] && !*edid[2])
     return NULL;
 
+  if (!settings->monitor_manager)
+    return NULL;
+
   outputs = flashback_monitor_manager_get_outputs (settings->monitor_manager,
                                                    &n_outputs);
 
@@ -1362,4 +1365,6 @@ gf_input_settings_set_display_config (GfInputSettings        *settings,
   settings->monitors_changed_id =
     g_signal_connect (settings->monitor_manager, "monitors-changed",
                       G_CALLBACK (monitors_changed_cb), settings);
+
+  monitors_changed_cb (monitor_manager, settings);
 }
