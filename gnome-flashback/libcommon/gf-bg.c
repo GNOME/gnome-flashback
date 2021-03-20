ab7b3b51437832b6034db6fd0b81cf555e85d0c5
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2021-03-20 17:58:50 +0200

    common: update copyright headers

--- gnome-flashback/libcommon/gf-bg.c
+++ gnome-flashback/libcommon/gf-bg.c
@@ -1,31 +1,26 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*-
-
-gnomebg.c: Object for the desktop background.
-
-Copyright (C) 2000 Eazel, Inc.
-Copyright (C) 2007-2008 Red Hat, Inc.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU Library General Public License as
-published by the Free Software Foundation; either version 2 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Library General Public License for more details.
-
-You should have received a copy of the GNU Library General Public
-License along with this program; if not, write to the
-Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301, USA.
-
-Derived from eel-background.c and eel-gdk-pixbuf-extensions.c by
-Darin Adler <darin@eazel.com> and Ramiro Estrugo <ramiro@eazel.com>
-
-Author: Soren Sandmann <sandmann@redhat.com>
-
-*/
+/*
+ * Copyright (C) 2000 Eazel, Inc.
+ * Copyright (C) 2007-2008 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Derived from eel-background.c and eel-gdk-pixbuf-extensions.c by
+ * Darin Adler <darin@eazel.com> and Ramiro Estrugo <ramiro@eazel.com>
+ *
+ * Author:
+ *   Soren Sandmann <sandmann@redhat.com>
+ */
 
 #include "config.h"
 #include "gf-bg.h"
