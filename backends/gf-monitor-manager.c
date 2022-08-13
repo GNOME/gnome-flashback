86c5a95a4008dec240a3fea3f934067813205978
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2022-08-13 14:20:06 +0300

    monitor-manager: make warning message less confusing
    Based on mutter commit:
    https://gitlab.gnome.org/GNOME/mutter/-/commit/f5887a6258453bbba658e5b7e201df1fde3ae841

--- backends/gf-monitor-manager.c
+++ backends/gf-monitor-manager.c
@@ -2305,7 +2305,7 @@ gf_monitor_manager_real_read_current_state (GfMonitorManager *manager)
 
       if (!gf_gpu_read_current (gpu, &error))
         {
-          g_warning ("Failed to read current KMS state: %s", error->message);
+          g_warning ("Failed to read current monitor state: %s", error->message);
           g_clear_error (&error);
         }
     }
